6832b0f1400344d7277c6ab72576a57adf480672
KMacdonald <kylemacdo01@gmail.com>
2025-02-27 10:08:40 -0500

    datagram packet

--- networking/packet.java
+++ networking/packet.java
@@ -1,18 +1,26 @@
 package networking;
 import java.net.Socket;
+import java.net.DatagramPacket;
 
 public class packet {
+    //Data
     String version;
     int length;
     String[] flags;
     Socket destSocket;
-    public packet(String version, int length, String[] flags, Socket destSocket){
+    DatagramPacket datagramPacket;
+
+    //Constructor
+    public packet(String version, int length, String[] flags, Socket destSocket, DatagramPacket datagramPacket){
         this.version = version;
         this.length = length;
         this.flags = flags;
         this.destSocket = destSocket;
+        this.datagramPacket = datagramPacket;
     }
 
+
+    //Getters and setters
     public String getVersion(){
         return this.version;
     }
